a658b33e34e3bbde81196de0a42247a996c880b3
Lukas <lukasmccain0@gmail.com>
2025-03-30 13:35:48 -0400

    Finally starting to make some functions.
    Also, decided to make our API for our stocks Alpha Avantage

--- app.py
+++ app.py
@@ -1,2 +1,25 @@
-def main():
-    return
+from flask import Flask
+import requests 
+
+# Gonna use Alpha advantage for API's
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return 'Index Page'
+
+"""
+This function returns the price of a stock based off of the stock tag requested
+
+Parameters:
+    stockTag: the tag of the stock requested from the user
+
+Returns:
+    str: a string of the stock price requested
+"""
+def getStock(stockTag:str) -> str:
+
+
+def getTags() -> list[str]:
